1b1146c301da8f8ac4d100f915a1c15e62a80c4f
jivkokirishev <jivkokirishev@gmail.com>
2018-06-06 19:12:27 +0300

    Creating methods neighbours_L2 and neighbours_inf, which will play major role in calculating the weights between pixels.

--- src/main.py
+++ src/main.py
@@ -2,6 +2,9 @@ import cv2
 from matplotlib import pyplot as plt
 import skimage
 import math
+
+from src.Pixel import Pixel
+
 try:
     from skimage import filters
 except ImportError:
@@ -9,12 +12,38 @@ except ImportError:
 
 
 def neighbours_L2(mat, y, x, distance = 1):
+    sh = mat.shape
+    out = []
+    for d in range(1, distance + 1):
+        if x + d < sh[1]:
+            pix = Pixel(y, x + d, mat[y, x + d], y * sh[1] + x + d)
+            out.append([pix, d])
 
-    return 0
+        if x - d >= 0:
+            pix = Pixel(y, x - d, mat[y, x - d], y * sh[1] + x - d)
+            out.append([pix, d])
+
+        if y + d < sh[0]:
+            pix = Pixel(y + d, x, mat[y + d, x], (y + d) * sh[1] + x)
+            out.append([pix, d])
+
+        if y - d >= 0:
+            pix = Pixel(y - d, x, mat[y - d, x], (y - d) * sh[1] + x)
+            out.append([pix, d])
+    return out
 
 def neighbours_inf(mat, y, x, distance = 1):
+    sh = mat.shape
+    out = []
 
-    return 0
+    for i in range(y - distance, y + distance + 1):
+        for j in range(x - distance, x + distance + 1):
+
+            if 0 <= i < sh[0] and 0 <= j < sh[1] and not (i == y and j == x):
+                pix = Pixel(i, j, mat[i, j], i * sh[1] + j)
+                d = max([abs(x - j), abs(y - i)])
+                out.append([pix, d])
+    return out
 
 def avg(mat, y, x):
 
